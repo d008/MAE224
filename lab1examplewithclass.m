ab8e2d0a36bb6c14b90e27ea79cf312ad8a7df43
mkfu <mkfu@princeton.edu>
2016-07-21 14:53:28 -0400

    Update lab1examplewithclass.m

--- lab1examplewithclass.m
+++ lab1examplewithclass.m
@@ -2,21 +2,35 @@
 
 %Enter access token here. This can be found from the settings of your Spark
 %Core account
-atoken = '';
+atoken = 'abc123';
 
 %Enter the core ID
 core =  'class1';
 g = Photon(core,atoken);
 g.getConnectedDevices()
-N = 40;
+
+%%
+N = 25;
 data = zeros(1,N);
+data2 = zeros(1,N);
 time = zeros(1,N);
-g.setInput('A4');
+
+g.setFreq(500);
 tic
+
+g.analogWrite('A4',0);
+    pause(1)
 for i = 1:N
-    i
-    data(i) = g.analogRead('A4');
+    i;
+    g.analogWrite('A4',10*i);
+    g.digitalWrite('D4',mod(i,2));
+    pause(1)
+    temp  = g.analogRead('A3')/4095*3.3
+    temp2  = g.digitalRead('D3')
+    data(i)  = temp;
+    data2(i)  = temp2;
     time(i) = toc;
-    pause(0.5)
+    pause(1)
 end
+
 plot(time,data)
