7037b758883caf916f2cb6703a60e3c17877f450
Matthew Fu <mkfu@princeton.edu>
2016-09-01 16:06:55 -0400

    new example

--- Source Files/MATLAB v1/lab1examplewithclass.m
+++ Source Files/MATLAB v1/lab1examplewithclass.m
@@ -1,37 +1,42 @@
 %% Read Data Example
-
-%Enter access token here. This can be found from the settings of your Spark
-%Core account
-atoken = 'abc123';
+%Enter access token below. This can be found in the settings of your Particle Account
+atoken = 'abc123'; %YOUR ACCESS TOKEN HERE
 
 %Enter the core ID
-core =  'class1';
-g = Photon(core,atoken);
-g.getConnectedDevices()
+core =  'class1'; %YOUR PHOTON ID OR NAME HERE
 
-%%
-N = 25;
+%Instantiates a new Photon object
+g = Photon(core,atoken);
+g.getConnectedDevices()'
+
+%% Create Empty arrays
+N = 20;
 data = zeros(1,N);
 data2 = zeros(1,N);
 time = zeros(1,N);
 
-g.setFreq(500);
-tic
 
-g.analogWrite('A4',0);
-g.attachServo('A5');
-    pause(1)
-for i = 1:N
-    i;
-    g.analogWrite('A4',10*i);
-    g.digitalWrite('D4',mod(i,2));
-    %pause(1)
-    temp  = g.analogRead('A3')/4095*3.3
-    temp2  = g.digitalRead('D3')
-    data(i)  = temp;
-    data2(i)  = temp2;
-    time(i) = toc;
-    %pause(1)
+%Check if the device is connected
+if g.getConnection
+    %Iterate from 1 to 10. Increment the analog write from 0 -255. Alternate
+    %the digital write between 1 and 0.  Connect A3 to A4 and D3 to D7.
+    tic
+    for i = 1:N
+        i
+        g.analogWrite('A4',floor(255/N)*i);
+        g.digitalWrite('D7',mod(i,2));
+        data(i)  = g.analogRead('A3')/4095*3.3;
+        data2(i)  = g.digitalRead('D3')*3.3;
+        time(i) = toc;
+    end
 end
-
-plot(time,data)
+%%  Plot that data
+figure(1)
+clf
+plot(time,data,'b-o')
+hold on
+plot(time,data2,'r-s');
+hold off
+xlabel('Time(s)')
+ylabel('Voltage')
+legend('Analog','Digital','location','southeast')
