44f605de2edf7a4a0596897e2edd1f95f85e0dbe
Mark Miller <millerma@princeton.edu>
2017-02-06 20:22:52 -0500

    some updates to cyl and af

---
+++ Lab Equipment and Etc/MRS_AIRFOIL_CODE.ino
@@ -0,0 +1,165 @@
+// This code reads in multiplexed pressure sensors for the MAE 224 LAB
+// Airfoil experiment and outputs them to the cloud in arrays
+//#include "application.h"
+
+
+//Mux control top pins
+int t0 = D0;
+int t1 = D1;
+int t2 = D2;
+int t3 = D3;
+//Mux control bottom pins
+int b0 = D4;
+int b1 = D5;
+int b2 = D6;
+int b3 = D7;
+
+
+//Mux Top control pins
+int topControl[] = {t0, t1, t2, t3};
+
+int tsig = A0; //Mux A "SIG" pin
+// Read these pins on MUX A
+
+//MUX Bottom Control Pins
+int bottomControl[] = {b0, b1, b2, b3};
+int bsig = A1; //MUX B "SIG" pin
+// Read these pins on MUX B
+  int muxChannel[16][4]={
+    {0,0,0,0}, //channel 0
+    {1,0,0,0}, //channel 1
+    {0,1,0,0}, //channel 2
+    {1,1,0,0}, //channel 3
+    {0,0,1,0}, //channel 4
+    {1,0,1,0}, //channel 5
+    {0,1,1,0}, //channel 6
+    {1,1,1,0}, //channel 7
+    {0,0,0,1}, //channel 8
+    {1,0,0,1}, //channel 9
+    {0,1,0,1}, //channel 10
+    {1,1,0,1}, //channel 11
+    {0,0,1,1}, //channel 12
+    {1,0,1,1}, //channel 13
+    {0,1,1,1}, //channel 14
+    {1,1,1,1}  //channel 15
+    };
+//Zeros, saved here so we can update them if needed outside the photon.m file    
+int ptz[16] = {2031,2028,2034,2030,2032,2042,2042,2055,2074,2066,2068,2066,2078,2073,2076,2070};  
+int pbz[16] = {2058,2058,2054,2049,2050,2051,2064,2051,2042,2152,2046,2050,2041,2048,2048,2041};
+//int ptz[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};  
+//int pbz[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+
+
+int avg = 200; //Milliseconds to average over
+int vals[32];//Storage for channel reads
+double aoa = 0.0; //Ang meas is with sparkfun ADXL335
+int inclinometer = A2;
+int incsense = 410; // Sensitivity of accelerometer in bits/1g (is ratiometric to supply voltage) 410 bits/g for Vs=3.33V
+int inczero = 2048; // Bit output when AoA is zero (i.e. 0g acceleration reading on x axis)
+
+char allPressures[621]; //String storage for Particle Cloud variables
+char temp[621];
+char finalPressures[621];
+int dum = 0;
+
+void setup(){
+    
+  Serial.begin(9600);
+  Particle.variable("Pressure",finalPressures);
+  Particle.variable("Angle",aoa);
+  pinMode(tsig,INPUT);
+  pinMode(bsig,INPUT);
+//Set up MUX A
+  for(int i = 0; i<4; i++)
+    {    
+      pinMode(topControl[i], OUTPUT);
+      digitalWrite(topControl[i], LOW);
+    }
+//Set up MUX B
+  for(int i = 0; i<4; i++)
+    {
+      pinMode(bottomControl[i], OUTPUT);
+      digitalWrite(bottomControl[i], LOW);
+    }
+
+}
+
+
+void loop(){
+    for(int i = 0; i < 32; i ++){
+      vals[i]=0;
+    }
+  dum = 0;    
+  sprintf(allPressures,"");
+  sprintf(temp,""); 
+
+  //Loop through and read all Muxer channels
+  for(int t = 0; t< avg;t++){
+  for(int i = 0; i < 16; i++){
+    vals[i] += (double)readMux(1,i);
+    vals[i+16] += (double)readMux(0,i);
+     }
+    }
+    sprintf(allPressures,"");
+    sprintf(temp,"");
+    for(int i = 0; i < 31; i ++){
+    sprintf(temp,"%s",allPressures);
+    if(i<16){
+       dum = round( vals[i]/avg) - ptz[i];
+    }
+    else{
+       dum = round(vals[i]/avg) - pbz[i-16];
+    }
+    sprintf(allPressures,"%s%d,",temp,dum);
+  }
+    sprintf(temp,"%s",allPressures);
+    dum = round(vals[31]/avg) - pbz[15];
+    sprintf(finalPressures,"%s%d",temp,dum);
+      
+  //sprintf(finalPressures,"%s",allPressures);
+  //Serial.print(finalPressures);
+  
+  //Read in angle of attack
+  int temp2 = analogRead(inclinometer);
+  aoa = (double)(90.0/incsense)*temp2 - (90.0/incsense)*inczero; //Output in degrees
+  //aoa = (((double)temp/4095.0-0.5)*2.0+0.01)/0.2;
+ Serial.print("Angle of Attack is ");
+ Serial.println(aoa);
+ 
+
+}
+
+int readMux(bool isTop, int channel){
+  //loop through the 4 sig
+  for(int i = 0; i < 4; i ++){
+    digitalWrite(topControl[i], muxChannel[channel][i]);
+    digitalWrite(bottomControl[i], muxChannel[channel][i]);
+
+  }
+   // Serial.println(channel);
+  //read the value at the SIG pin
+  int val;
+  if(isTop)
+  {
+  val = analogRead(tsig);
+  }
+  else
+  {
+  val = analogRead(bsig);
+  }
+  //return the value
+  Serial.println(val);
+  return val;
+}
+
+
+
+
+
+
+
+
+
+
+
