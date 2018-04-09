d67ecb535ea2946cecc7de194e403eb25e381f06
anovoselov <agn@princeton.edu>
2018-04-09 13:55:49 -0400

    Update mrs-airfoil-code.ino

--- Lab 4 Shells/mrs-airfoil-code.ino
+++ Lab 4 Shells/mrs-airfoil-code.ino
@@ -2,17 +2,29 @@
 // Airfoil experiment and outputs them to the cloud in arrays
 //#include "application.h"
 
-//Mux A control pins
-int acontrol[] = {D0, D1, D2, D3};
-int asig = A0; //Mux A "SIG" pin
-// Read these pins on MUX A
-int apins[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
-//MUX B Control Pins
-int bcontrol[] = {D4, D5, D6, D7};
+//Mux control top pins
+int t0 = D0;
+int t1 = D1;
+int t2 = D2;
+int t3 = D3;
+//Mux control bottom pins
+int b0 = D4;
+int b1 = D5;
+int b2 = D6;
+int b3 = D7;
+
+
+//Mux Top control pins
+int topControl[] = {t0, t1, t2, t3};
+
+int tsig = A0; //Mux A "SIG" pin
+// Read these pins on MUX A
+
+//MUX Bottom Control Pins
+int bottomControl[] = {b0, b1, b2, b3};
 int bsig = A1; //MUX B "SIG" pin
 // Read these pins on MUX B
-int bpins[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
   int muxChannel[16][4]={
     {0,0,0,0}, //channel 0
     {1,0,0,0}, //channel 1
@@ -31,94 +43,93 @@ int bpins[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
     {0,1,1,1}, //channel 14
     {1,1,1,1}  //channel 15
     };
-// Find total number of pins to read
-int sza = sizeof(apins) / sizeof(int);
-int szb = sizeof(bpins) / sizeof(int);
-int szt = sza + szb;
+//Zeros, saved here so we can update them if needed outside the photon.m file    
+int ptz[16] = {2029,2028,2033,2029,2030,2042,2044,2056,2056,2048,2051,2048,2061,2056,2057,2051};  
+int pbz[16] = {2060,2060,2055,2049,2048,2052,2063,2053,2043,2132,2047,2050,2041,2040,2051,2041};
+//int ptz[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};  
+//int pbz[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-int t = 1; //Milliseconds to average over
+
+
+int avg = 500; //Milliseconds to average over
 int vals[32];//Storage for channel reads
 double aoa = 0.0; //Ang meas is with sparkfun ADXL335
 int inclinometer = A2;
 int incsense = 410; // Sensitivity of accelerometer in bits/1g (is ratiometric to supply voltage) 410 bits/g for Vs=3.33V
-int inczero = 2048; // Bit output when AoA is zero (i.e. 0g acceleration reading on x axis)
-
+//int inczero = 0; // Bit output when AoA is zero (i.e. 0g acceleration reading on x axis)
+int inczero = 2008; // Visual zero
+//int inczero = 1997; // Using force balance at Lift = 0
 char allPressures[621]; //String storage for Particle Cloud variables
 char temp[621];
+char finalPressures[621];
+int dum = 0;
 
 void setup(){
     
   Serial.begin(9600);
-  Particle.variable("Pressure",allPressures);
-  Particle.variable("numbchannel",szt);
+  Particle.variable("Pressure",finalPressures);
   Particle.variable("Angle",aoa);
-  pinMode(asig,INPUT);
+  pinMode(tsig,INPUT);
   pinMode(bsig,INPUT);
 //Set up MUX A
   for(int i = 0; i<4; i++)
     {    
-      pinMode(acontrol[i], OUTPUT);
-      digitalWrite(acontrol[i], HIGH);
+      pinMode(topControl[i], OUTPUT);
+      digitalWrite(topControl[i], LOW);
     }
 //Set up MUX B
   for(int i = 0; i<4; i++)
     {
-      pinMode(bcontrol[i], OUTPUT);
-      digitalWrite(bcontrol[i], HIGH);
+      pinMode(bottomControl[i], OUTPUT);
+      digitalWrite(bottomControl[i], LOW);
     }
-  
+
 }
 
 
 void loop(){
-    
+    for(int i = 0; i < 32; i ++){
+      vals[i]=0;
+    }
+  dum = 0;    
   sprintf(allPressures,"");
   sprintf(temp,""); 
 
-  //Loop through and read all MUX A channels
-  for(int i=0; i<(sza); i++)
-    {
-        vals[i]=0;
-        vals[i] = (double)readMux(apins[i], asig, acontrol);
-        /*Serial.print("Value for MUX A at channel ");
-        Serial.print(apins[i]);
-        Serial.print(" is: ");
-        Serial.println( vals[i] );
-        delay(500);
-        Serial.flush();
-        */
-
-     
+  //Loop through and read all Muxer channels
+  for(int t = 0; t< avg;t++){
+  for(int i = 0; i < 16; i++){
+    vals[i] += (double)readMux(1,i);
+    vals[i+16] += (double)readMux(0,i);
+     }
     }
-    
-  //Loop through and read all MUX B channels
-  for(int i = 0; i < (szb); i++){
-    vals[i+sza]=0;
-      vals[i+sza] = (double)readMux(bpins[i], bsig, bcontrol);
-      /*Serial.print("Value for MUX B at channel ");
-      Serial.print( bpins[i] );
-      Serial.print(" is : ");
-      Serial.println( vals[i+sza] );
-      delay(100);
-      Serial.flush();
-      */
-
+    sprintf(allPressures,"");
+    sprintf(temp,"");
+    for(int i = 0; i < 31; i ++){
+    sprintf(temp,"%s",allPressures);
+    if(i<16){
+       dum = round( vals[i]/avg) - ptz[i];
+    }
+    else{
+       dum = round(vals[i]/avg) - pbz[i-16];
+    }
+    sprintf(allPressures,"%s%d,",temp,dum);
   }
-  
-  //Write values to string      
-  for(int i = 0; i < szt-1; i++){
-      sprintf(temp,"%s",allPressures);
-      sprintf(allPressures,"%s%d,",temp,vals[i]);  
-  }
-  //Write last value into final array, no delimiter afterwards
-  sprintf(allPressures,"%s%d",allPressures,vals[szt]);
-  
+    sprintf(temp,"%s",allPressures);
+    dum = round(vals[31]/avg) - pbz[15];
+    sprintf(finalPressures,"%s%d",temp,dum);
+      
   //sprintf(finalPressures,"%s",allPressures);
   //Serial.print(finalPressures);
   
   //Read in angle of attack
-  int temp2 = analogRead(inclinometer);
-  aoa = (double)(90.0/incsense)*temp2 - (90.0/incsense)*inczero; //Output in degrees
+  int temp2 = 0;
+  for(int t = 0; t< avg; t++){
+  temp2 += analogRead(inclinometer);
+  }
+  //aoa = temp2/avg; // Use this to read aoa when setting bit-value zero
+  
+  aoa = (double)(90.0/incsense)*temp2/avg - (90.0/incsense)*inczero; //Output in degrees
+  
   //aoa = (((double)temp/4095.0-0.5)*2.0+0.01)/0.2;
  Serial.print("Angle of Attack is ");
  Serial.println(aoa);
@@ -126,51 +137,25 @@ void loop(){
 
 }
 
-int readMux(int channel, int SIG_pin, int controlPin[]){
+int readMux(bool isTop, int channel){
   //loop through the 4 sig
   for(int i = 0; i < 4; i ++){
-    digitalWrite(controlPin[i], muxChannel[channel][i]);
-    digitalWrite(controlPin[i], muxChannel[channel][i]);
+    digitalWrite(topControl[i], muxChannel[channel][i]);
+    digitalWrite(bottomControl[i], muxChannel[channel][i]);
 
   }
-    delay(100);
+   // Serial.println(channel);
   //read the value at the SIG pin
   int val;
-  if(SIG_pin == A0)
+  if(isTop)
   {
-  val = analogRead(A1);
+  val = analogRead(tsig);
   }
   else
   {
-  val = analogRead(A1);
+  val = analogRead(bsig);
   }
   //return the value
   Serial.println(val);
   return val;
-}
-
-
-int readMux2(int channel, int SIG_pin, int controlPin[]){
-
-  //loop through the 4 pins
-  for(int i = 0; i < 4; i ++){
-      int dum = muxChannel[channel][i];
-      int dum2 = controlPin[i];
-    digitalWrite(dum2, dum);
-    Serial.print(dum);
-    //digitalWrite(controlPin[i], 0);
-  }
-Serial.println(channel);
-  //read the value at the SIG pin and average over t samples
-  int val = 0;
-  for(int i=0; i<t; i++)
-        {
-            val += analogRead(SIG_pin);
-              //delay(1); //Wait 1ms between samples
-        }    
-        Serial.println(val);    
-    int fval = int(val/t);
-  //return the value
- return analogRead(SIG_pin);
-  //return val;
 }
